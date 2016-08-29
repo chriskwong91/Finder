2a642b574507c0b8e6c8257312db673852b05b2e
Christopher Wong <chriskwong91@gmail.com>
2016-08-29 16:59:29 -0700

    Add layout of FavoriteList and FavoriteListItem

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,6 +7,8 @@ class App extends React.Component {
 
   }
 
+
+
   render() {
     return (
       <div>
---
+++ client/components/FavoriteList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+let FavoriteList = () => {
+  return (
+
+  );
+};
+
+export default FavoriteList;
